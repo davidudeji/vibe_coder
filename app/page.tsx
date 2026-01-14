6d0cd2b7692fbebc7d4e67104bb92529d10d25bf
DavidUdeji <davidcudeji@gmail.com>
2026-01-14 03:38:16 -0600

    added logout component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import UserButton from "@/modules/auth/components/user-button";
 import SignOutNav from "./components/signout-nav-client";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <SignOutNav />
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <div className="p-6 bg-white rounded shadow"><h1 className="text-xl">Welcome</h1></div>
+        <UserButton/>
       </div>
     </>
   );
