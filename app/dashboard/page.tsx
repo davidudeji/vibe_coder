20976edcdd628c8ba6884bebd199707a6536543f
DavidUdeji <davidcudeji@gmail.com>
2026-01-15 08:39:30 -0600

    added dashboard table ui logic

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,30 @@
 import AddNewButton from '@/modules/dashboard/components/add-new';
 import AddRepo from '@/modules/dashboard/components/add-repo';
 import React from 'react';
+import EmptyState from './empty-state';
+import { getAllPlaygroundForUser } from '@/modules/dashboard/actions';
+import ProjectTable from './project-table';
 
-const DashboardPage = () => {
+const DashboardPage = async() => {
+
+  const playgrounds = await getAllPlaygroundForUser();
   return (
     <div className='flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-full'>
         <AddNewButton/>
         <AddRepo/>
+       
+      </div>
+
+      <div className='mt-10 flex flex-col justify-center items-center w-full'>
+      {playgrounds && playgrounds.length === 0 ? (
+         <EmptyState/>
+      ) : (
+        <ProjectTable projects={playgrounds || []}
+        onDeleteProject={() => {}}
+        onUpdateProject={() => {}}
+        onDuplicateProject={() => {}} />
+      )}
       </div>
     </div>
   );
